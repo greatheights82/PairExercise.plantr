d991e530a45ae67ba93cd31d98b970f50ad159e3
Zachary Marszale <zmarszal@ameritech.net>
2019-01-22 15:53:59 -0600

    plantr initial commit

---
+++ seed.js
@@ -0,0 +1,12 @@
+const db = require('./models');
+
+db.sync({force: true})
+.then(() => {
+  console.log('database is running')
+})
+.catch((err) => {
+  console.log(err)
+})
+.finally(() => {
+  db.close();
+})
